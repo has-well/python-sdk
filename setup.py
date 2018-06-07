ba16df71ec809e490f80a1fd4c86fdfbcd429c20
dimoncheg12 <dimoncheg12@mail.ru>
2018-06-07 13:07:14 +0300

    init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 from cloudipsp.configuration import __version__
 
-with open('README.md', 'r') as f:
+with open('./README.md', 'r') as f:
     readme = f.read()
 
 requires_list = [
